f883cead2b7dcaa8c1b0bd9d470a4801e0b4ecec
HusnaAfzal <afzalibrahim0323@gmail.com>
2024-07-21 18:55:27 +0500

    Add files via upload

---
+++ assignmenthusna.py
@@ -0,0 +1,18 @@
+
+#calculate the area of triangle using the formula (0.5*base*height)
+base=float(input("Enter the base length of the triangle:"))
+height=float(input("Enter the height of the triangle:"))
+area=0.5*base*height
+print("The area of the triangle is:",area)
+
+#Calculate the area of square using the formula(side*side)
+S= float(input("Enter Side of the square"))
+area=S*S
+print ("the area of Square is",area)
+
+#Find the area of a circle with a radius of 7 units Formula(π*radius*radius)
+
+import math
+radius= 7
+area=math.pi*(radius*radius)
+print("the area of circle with radius of 7 units is",area)
